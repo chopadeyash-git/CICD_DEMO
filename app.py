775f6654bdfdc752df0b000dff947aff818741a2
chopadeyash-git <your-email@example.com>
2026-04-19 17:19:40 +0530

    fix test output

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to world of cicd!"
+    return b"Welcome to world of cicd!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
